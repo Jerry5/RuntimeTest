9a6eb35b8548e0626cdc8ee84404bb4d873d3974
JiaFang <jerry@wuzhiyuandeimac.local>
2016-05-13 10:16:55 +0800

    Initial Commit

---
+++ RuntimeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeTest
+//
+//  Created by 武志远 on 16/5/13.
+//  Copyright © 2016年 武志远. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeTest
+//
+//  Created by 武志远 on 16/5/13.
+//  Copyright © 2016年 武志远. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
